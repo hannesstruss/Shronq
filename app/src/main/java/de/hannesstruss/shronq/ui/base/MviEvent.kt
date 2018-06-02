9010062a97557110292d8bb2b69a085735a58037
Hannes Struß <x@hannesstruss.de>
2018-06-02 19:52:15 +0200

    Data class events (for nicer toStrings) and no lazy

--- app/src/main/java/de/hannesstruss/shronq/ui/base/MviEvent.kt
+++ app/src/main/java/de/hannesstruss/shronq/ui/base/MviEvent.kt
@@ -3,9 +3,8 @@ package de.hannesstruss.shronq.ui.base
 import io.reactivex.Observable
 
 sealed class MviEvent<ChangeT, EffectT> {
-  class Change<ChangeT>(val change: ChangeT) : MviEvent<ChangeT, Nothing>()
-  class Effect<EffectT>(val effect: EffectT) : MviEvent<Nothing, EffectT>()
-  object Neither : MviEvent<Nothing, Nothing>()
+  data class Change<ChangeT>(val change: ChangeT) : MviEvent<ChangeT, Nothing>()
+  data class Effect<EffectT>(val effect: EffectT) : MviEvent<Nothing, EffectT>()
 
   companion object {
     fun nothing(): Observable<MviEvent<Nothing, Nothing>> = Observable.empty()
--- app/src/main/java/de/hannesstruss/shronq/ui/home/HomeFragment.kt
+++ app/src/main/java/de/hannesstruss/shronq/ui/home/HomeFragment.kt
@@ -13,9 +13,7 @@ class HomeFragment : BaseFragment<HomeState, HomeIntent, HomeEffect, HomeViewMod
   override val layout = R.layout.home_fragment
   override val viewModelClass = HomeViewModel::class.java
 
-  override val intents by lazy {
-    btn_go_to_insert.clicks().map<HomeIntent> { HomeIntent.InsertWeight }
-  }
+  override val intents get() = btn_go_to_insert.clicks().map<HomeIntent> { HomeIntent.InsertWeight }
 
   override fun render(state: HomeState) {
     chart.measurements = state.measurements
