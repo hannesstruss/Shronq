833557dbd2a987c71bf397350ab4d997bc3bc7db
Hannes Struß <x@hannesstruss.de>
2019-03-31 14:28:01 +0200

    Drafting

--- app/src/main/java/de/hannesstruss/shronq/ui/base/MviViewModel2.kt
+++ app/src/main/java/de/hannesstruss/shronq/ui/base/MviViewModel2.kt
@@ -6,9 +6,35 @@ import io.reactivex.Observable
 import kotlinx.coroutines.delay
 import timber.log.Timber
 import java.time.LocalTime
+import java.util.concurrent.TimeUnit
+
+/*
+ * TODO
+ *
+ * Questions:
+ *
+ * # Is `enterState` cool, or do we need atomic state transitions ("Actions")?
+ *
+ * Often, actions merely describe setting a field on a state (`ShowLoading(true)`).
+ * This in itself is already not ideal, rather an action should describe an event,
+ * e.g. `StartedRefreshing` which the state/reducer can interpret as `loading = true`.
+ *
+ * Pro of having actions is that the set of state transitions is limited and
+ * visible at one glance when looking at the action `sealed class`.
+ *
+ * Pro of having `enterState` is increased flexibility and less ceremony to get a screen
+ * up and running.
+ *
+ * A middle ground might be `State` having "action methods" like `fun incrementCounter(): State`.
+ *
+ * Another middle ground might be allowing a special `EnterState(val state: State)` action
+ * for certain engines. This encodes the state at the time of action creation though, so
+ * instead we should use `typealias MyAction = (State) -> State`
+ */
 
 abstract class MviViewModel2<StateT : Any, IntentT : Any> {
-  val states: Observable<StateT> = TODO()
+  val states: Observable<out StateT> = TODO()
+  val intents: Observable<out IntentT> = TODO()
 
   abstract val engine: MviEngine
 
@@ -21,13 +47,13 @@ interface MviEngine
 
 interface EngineContext<StateT, IntentT> {
   fun onInit(block: suspend () -> Unit)
-  fun <T : IntentT> on(block: suspend IntentContext<StateT, T>.(T) -> Unit)
-  fun <T : IntentT> onFirst(block: suspend IntentContext<StateT, T>.(T) -> Unit)
-  fun <T : IntentT> streamOf(block: StreamContext<StateT>.(Observable<T>) -> Observable<StateEditor<StateT>>)
+  fun <T : IntentT> on(block: suspend IntentContext<StateT>.(T) -> Unit)
+  fun <T : IntentT> onFirst(block: suspend IntentContext<StateT>.(T) -> Unit)
+  fun <T : IntentT> streamOf(block: StreamContext<StateT>.(Observable<T>) -> StreamContextToken)
   fun externals(block: ExternalsContext<StateT>.() -> Unit)
 }
 
-interface IntentContext<StateT, IntentT> {
+interface IntentContext<StateT> {
   fun enterState(block: StateEditorContext<StateT>.() -> StateT)
 }
 
@@ -38,9 +64,12 @@ interface StateEditorContext<StateT> {
 typealias StateEditor<StateT> = (StateT) -> StateT
 
 interface StreamContext<StateT> {
-  fun <T> Observable<T>.onNextEnterState(block: StateEditorContext<StateT>.(T) -> StateT): Observable<StateEditor<StateT>>
-  fun <T> Observable<T>.dontEmitStates(): Observable<out StateT> = ignoreElements().toObservable()
+  fun <T> Observable<T>.hookUp(): StreamContextToken
+  fun <T> Observable<T>.hookUp(block: IntentContext<StateT>.(T) -> Unit): StreamContextToken
 }
+
+// Force usage of `hookUp` in `streamOf` TODO: make more forceful and less abusable
+interface StreamContextToken
 
 interface ExternalsContext<StateT> {
   fun <T> Observable<T>.hookUp(block: suspend ExternalsItemContext<StateT>.(T) -> Unit)
@@ -50,8 +79,11 @@ interface ExternalsItemContext<StateT> {
   fun enterState(block: StateEditorContext<StateT>.() -> StateT)
 }
 
+
 data class MyState(val counter: Int = 0, val secondsSum: Int = 0) {
   fun incrementCounter() = copy(counter = counter + 1)
+  fun decrementCounter() = copy(counter = counter - 1)
+  fun addSeconds(seconds: Int) = copy(secondsSum = secondsSum + seconds)
 }
 
 sealed class MyIntent {
@@ -71,6 +103,9 @@ class MyViewModel : MviViewModel2<MyState, MyIntent>() {
       Timber.d("Hello again!")
     }
 
+    // TODO: nested view model engines
+    // nest(myNestedEngine) { nestedState -> ... }
+
     on<CountUp> {
       delay(1000)
       enterState { state.incrementCounter() }
@@ -78,22 +113,24 @@ class MyViewModel : MviViewModel2<MyState, MyIntent>() {
 
     streamOf<CountDown> { intents ->
       intents
-          .onNextEnterState {
-            state.copy(counter = state.counter - 1)
+          .sample(1, TimeUnit.SECONDS)
+          .hookUp {
+            enterState { state.decrementCounter() }
           }
     }
 
     streamOf<CountUp> { intents ->
       intents
+          .sample(1, TimeUnit.SECONDS)
           .doOnNext { Timber.d("Here we're just doing side effects, for whatever reason") }
-          .dontEmitStates()
+          .hookUp()
     }
 
     externals {
       MyTimeService.times
           .map { it.second }
           .hookUp {
-            enterState { state.copy(secondsSum = state.secondsSum + it) }
+            enterState { state.addSeconds(it) }
           }
     }
   }
