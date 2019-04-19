c8b4aeaaecd9ea85bc12f0371565a3da689ebdbe
Hannes Struß <x@hannesstruss.de>
2019-04-19 17:31:55 +0200

    Don't use GlobalScope

--- app/src/main/java/de/hannesstruss/shronq/ui/notifications/LunchNotificationPublisher.kt
+++ app/src/main/java/de/hannesstruss/shronq/ui/notifications/LunchNotificationPublisher.kt
@@ -4,17 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import de.hannesstruss.shronq.di.AppComponent
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class LunchNotificationPublisher : BroadcastReceiver() {
   @Inject lateinit var lunchNotification: LunchNotification
+  private val scope = CoroutineScope(Dispatchers.Main)
 
   override fun onReceive(context: Context, intent: Intent?) {
     AppComponent.get(context).inject(this)
 
-    GlobalScope.launch {
+    scope.launch {
       lunchNotification.triggerNow()
     }
   }
